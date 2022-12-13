a879676f88b0f3f7e1fa7d38e55d963159c5d989
Arsir <arfansiregar.33@gmail.com>
2022-12-13 11:07:33 +0700

    Invalid Javascript Module (Standard Library)

---
+++ standard-library.js
@@ -0,0 +1,13 @@
+import os from "os";
+
+console.info(os.platform());
+console.table(os.cpus());
+
+/**
+ * Problem: 
+ * Invalid Javascript Module
+ * 
+ * Solution:
+ * Change Extension from js to mjs
+ * 
+ * **/
